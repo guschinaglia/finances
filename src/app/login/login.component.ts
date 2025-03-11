47e43a8c925d7bf8c8475b5bc6ee5e9250b0f2a1
gus-chinaglia <chinaglia.gustavo@gmail.com>
2025-03-11 20:32:11 -0300

    Fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CardModule, InputTextModule, FormsModule],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
+  username: string | undefined;
+  password: string | undefined;
 }
